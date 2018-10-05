df95b568540eb2caae89e631f571e935c9506445
ohahthink <ohahthink@gmail.com>
2018-10-05 16:44:08 +0900

    Mad Snake Game

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -6,9 +6,10 @@ import {ROWS, COLS} from './config';
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
   this.joints = [
+    {x: 4, y: 0},
+    {x: 3, y: 0},
     {x: 2, y: 0},
     {x: 1, y: 0},
-    {x: 0, y: 0},
   ];
 
   // 먹이의 좌표
@@ -17,21 +18,29 @@ function SnakeGameLogic() {
 
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
+  
   console.log('up');
 }
 
 SnakeGameLogic.prototype.down = function() {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
+  
   console.log('down');
 }
 
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
+  this.joints.map(function (item) {
+    item.x--
+  })
   console.log('left');
 }
 
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
+  this.joints.map(function(item){
+    item.x++
+  })
   console.log('right');
 }
 
